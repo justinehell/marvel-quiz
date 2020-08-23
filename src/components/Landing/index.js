2c7bf3c52f6e1a029198ed5556a73b4fe8d230c4
justinehell <hell.justine@gmail.com>
2020-08-23 11:26:23 +0200

    Landing button and Wolverine animation using useRef

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const Landing = () => {
-  return <main className="welcomePage"></main>;
+  const [btn, setBtn] = useState(false);
+
+  const refWolverine = useRef(null);
+
+  useEffect(() => {
+    refWolverine.current.classList.add("startingImg");
+    setTimeout(() => {
+      refWolverine.current.classList.remove("startingImg");
+      setBtn(true);
+    }, 1000);
+  }, []);
+
+  const displayBtn = btn && (
+    <>
+      <div className="leftBox">
+        <button className="btn-welcome">Inscription</button>
+      </div>
+      <div className="rightBox">
+        <button className="btn-welcome">Connexion</button>
+      </div>
+    </>
+  );
+
+  return (
+    <main ref={refWolverine} className="welcomePage">
+      {displayBtn}
+    </main>
+  );
 };
 
 export default Landing;
