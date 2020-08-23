0b0c95b12f7f0d701081e365852ed5cc9df0d8da
justinehell <hell.justine@gmail.com>
2020-08-23 13:38:50 +0200

    instantiation of Firebase class via Context

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/index";
 import * as serviceWorker from "./serviceWorker";
+import Firebase, { FirebaseContext } from "./components/Firebase/";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
