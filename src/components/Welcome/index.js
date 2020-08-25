4760b7feb467c355ee5279f309718cffa90304d2
justinehell <hell.justine@gmail.com>
2020-08-25 17:43:27 +0200

    User session management via Firebase - show path welcome or redirect to login

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { FirebaseContext } from "../Firebase";
+
 import Logout from "../Logout";
 import Quiz from "../Quiz";
 
-const Welcome = () => {
-  return (
+const Welcome = (props) => {
+  const [userSession, setUserSession] = useState(null);
+
+  const firebase = useContext(FirebaseContext);
+
+  useEffect(() => {
+    let listener = firebase.auth.onAuthStateChanged((user) => {
+      user ? setUserSession(user) : props.history.push("/");
+    });
+
+    return () => {
+      listener();
+    };
+  }, []);
+
+  return userSession === null ? (
+    <>
+      <div className="loader">
+        <p className="loaderText">load</p>
+      </div>
+    </>
+  ) : (
     <div className="quiz-bg">
       <div className="container">
         <Logout />
